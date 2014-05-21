089f3b4d440380436c92e2672bccbb1a1e60215a
Philip Montgomery <pmontgom@broadinstitute.org>
2014-05-21 11:14:09 -0400

    Converted tabs->spaces in flock_support.R Added additional subdir in tasks dir when jobid > 1000 Fixed bug where job with no external id wouldn’t be considered finished

--- src/flock_support.R
+++ src/flock_support.R
@@ -1,60 +1,64 @@
 # TODO: when submitting, need to check *.finished exists.  If so, delete it.
 
 flock.run <- function(inputs, task_script_name, gather_script_name=NULL, shared_params=NULL, gather_params=NULL, script_path=NULL, run_dir=NULL) {
-	if(is.null(script_path)) {
-		script_path = Sys.getenv("FLOCK_HOME");
-		stopifnot(script_path != '');
-	}
+  if(is.null(script_path)) {
+    script_path = Sys.getenv("FLOCK_HOME");
+    stopifnot(script_path != '');
+  }
 
-	if(is.null(run_dir)) {
-		run_dir = Sys.getenv("FLOCK_RUN_DIR")
-		stopifnot(run_dir != '');
-	}
+  if(is.null(run_dir)) {
+    run_dir = Sys.getenv("FLOCK_RUN_DIR")
+    stopifnot(run_dir != '');
+  }
 
   task.dir <- 'tasks';
 
-	dir.create(paste(run_dir, '/', task.dir, sep=''), recursive=TRUE);
-	shared_params_file = paste(run_dir, '/',task.dir,'/shared_params.Rdata', sep='');
-	save(shared_params, file=shared_params_file)
-	
-	created.jobs <- list()
-	submit_command <- function(group, name, cmd) {
-		fileConn <- file(paste(run_dir, '/', task.dir, '/', name, sep=''))
-		writeLines(cmd, fileConn)
-		close(fileConn)
+  dir.create(paste(run_dir, '/', task.dir, sep=''), recursive=TRUE);
+  shared_params_file = paste(run_dir, '/',task.dir,'/shared_params.Rdata', sep='');
+  save(shared_params, file=shared_params_file)
+  
+  created.jobs <- list()
+  submit_command <- function(group, name, cmd) {
+    fileConn <- file(paste(run_dir, '/', task.dir, '/', name, sep=''))
+    writeLines(cmd, fileConn)
+    close(fileConn)
     
     created.jobs[[length(created.jobs)+1]] = paste(group, ' ', paste(task.dir, '/', dirname(name), sep=''), sep='')
     created.jobs <<- created.jobs
-	}
+  }
 
-	id.fmt.str = sprintf("%%0%.0f.0f", log(length(inputs))/log(10));
-	job_details = list()
-	for(job.index in 1:length(inputs)) {
-		job.id = sprintf(id.fmt.str, job.index);
-		param = inputs[job.index];
-		job_dir = paste(run_dir, '/', task.dir, '/', job.id, sep='');
-		dir.create(job_dir, recursive=TRUE);
-		input_file = paste(job_dir, '/input.Rdata', sep='')
-		output_file = paste(job_dir, '/output.Rdata', sep='')
-		script_name = task_script_name;
-		completion_file = paste(job_dir, '/finished-time.txt', sep='')
-		save(run_dir, job_dir, input_file, output_file, script_name, param, completion_file, file=input_file)
-		submit_command('1', paste(job.id, '/task.sh', sep=''), paste('exec R --vanilla --args ', shared_params_file, ' ', input_file, ' < ', script_path, '/execute_task.R', sep=''))
-		job_details[[length(job_details)+1]] = list(run_dir=run_dir, job_dir=job_dir, input_file=input_file, output_file=output_file, script_name=script_name, param=param)
-	}
+  id.fmt.str = sprintf("%%0%.0f.0f", ceiling(log(length(inputs))/log(10)));
+  job_details = list()
+  for(job.index in 1:length(inputs)) {
+    job.id = sprintf(id.fmt.str, job.index);
+    job.subdir = job.id
+    if(nchar(job.id) > 3) {
+      job.subdir = paste(substr(job.subdir, 1, nchar(job.subdir)-3), '/', job.id, sep='')
+    }
+    param = inputs[job.index];
+    job_dir = paste(run_dir, '/', task.dir, '/', job.subdir, sep='');
+    dir.create(job_dir, recursive=TRUE);
+    input_file = paste(job_dir, '/input.Rdata', sep='')
+    output_file = paste(job_dir, '/output.Rdata', sep='')
+    script_name = task_script_name;
+    completion_file = paste(job_dir, '/finished-time.txt', sep='')
+    save(run_dir, job_dir, input_file, output_file, script_name, param, completion_file, file=input_file)
+    submit_command('1', paste(job.subdir, '/task.sh', sep=''), paste('exec R --vanilla --args ', shared_params_file, ' ', input_file, ' < ', script_path, '/execute_task.R', sep=''))
+    job_details[[length(job_details)+1]] = list(run_dir=run_dir, job_dir=job_dir, input_file=input_file, output_file=output_file, script_name=script_name, param=param)
+  }
 
-	dir.create(paste(run_dir, '/',task.dir,'/gather', sep=''), recursive=TRUE);
-	gather_input_file = paste(run_dir, '/',task.dir,'/gather/input.Rdata', sep='')
-	completion_file = paste(run_dir, '/',task.dir,'/gather/finished-time.txt', sep='')
-	param = gather_params;
-	script_name = gather_script_name;
-	save(run_dir, job_dir, job_details, param, script_name, completion_file, file=gather_input_file)
-	submit_command('2', 'gather/task.sh', paste('exec R --vanilla --args ', shared_params_file, ' ', gather_input_file, ' < ', script_path, '/execute_task.R', sep=''))
+  dir.create(paste(run_dir, '/',task.dir,'/gather', sep=''), recursive=TRUE);
+  gather_input_file = paste(run_dir, '/',task.dir,'/gather/input.Rdata', sep='')
+  completion_file = paste(run_dir, '/',task.dir,'/gather/finished-time.txt', sep='')
+  param = gather_params;
+  script_name = gather_script_name;
+  save(run_dir, job_dir, job_details, param, script_name, completion_file, file=gather_input_file)
+  submit_command('2', 'gather/task.sh', paste('exec R --vanilla --args ', shared_params_file, ' ', gather_input_file, ' < ', script_path, '/execute_task.R', sep=''))
 
-	# write the list of task scripts
-	fileConn <- file(paste(run_dir, '/',task.dir,'/task_dirs.txt', sep=''))
-	#print(created.jobs);
-	#print(unlist(created.jobs));
-	writeLines(unlist(created.jobs), fileConn)
-	close(fileConn)
+  # write the list of task scripts
+  fileConn <- file(paste(run_dir, '/',task.dir,'/task_dirs.txt', sep=''))
+  #print(created.jobs);
+  #print(unlist(created.jobs));
+  writeLines(unlist(created.jobs), fileConn)
+  close(fileConn)
 }
---
+++ test/square_script/run_many_tasks.R
@@ -0,0 +1,1 @@
+flock.run(seq(1100), "test/square_script/square.R", "test/square_script/sum.R")
