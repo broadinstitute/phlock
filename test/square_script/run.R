d101813d69d178bf33d28faf4449c48e8cfc41d8
Philip Montgomery <pmontgom@broadinstitute.org>
2014-05-23 15:30:25 -0400

    Changed options to be loaded from yaml config files

--- test/square_script/run.R
+++ test/square_script/run.R
@@ -1,1 +1,3 @@
-flock.run(c(1,2), "test/square_script/square.R", "test/square_script/sum.R")
+do.run <- function(values) {
+	flock.run(values, "test/square_script/square.R", "test/square_script/sum.R")
+}
