df38d56058f00bef3f3a9277f7d08e907edd526e
Philip Montgomery <pmontgom@broadinstitute.org>
2017-06-09 11:29:27 -0400

    added r4 instance types

--- ui/instance_types.py
+++ ui/instance_types.py
@@ -1,1 +1,16 @@
-cpus_per_instance = {u'm3.large': 2, u'r3.4xlarge': 16, 'm1.small': 1, u'm3.2xlarge': 8, u'r3.2xlarge': 8, u'c3.2xlarge': 8, u'c4.8xlarge': 36, u'c3.xlarge': 4, 'm1.large': 2, u'hs1.8xlarge': 16, u'c3.8xlarge': 32, u'c4.xlarge': 4, u'c3.4xlarge': 16, u't2.medium': 2, u'i2.8xlarge': 32, u'c4.4xlarge': 16, u't2.micro': 1, u'g2.2xlarge': 8, u'r3.8xlarge': 32, u'c4.2xlarge': 8, u't2.small': 1, 'm1.medium': 1, u'r3.large': 2, u'i2.xlarge': 4, u'm3.medium': 1, u'i2.4xlarge': 16, u'r3.xlarge': 4, u'c4.large': 2, u'i2.2xlarge': 8, u'c3.large': 2, 'm1.xlarge': 4, u'm3.xlarge': 4}
+cpus_per_instance = {u'm3.large': 2, 
+u'r3.4xlarge': 16, 
+u'r4.4xlarge': 16, 
+u'r3.2xlarge': 8, 
+u'r4.2xlarge': 8, 
+u'r3.8xlarge': 32, 
+u'r4.8xlarge': 32,
+u'r3.large': 2, 
+u'r4.large': 2, 
+u'r3.xlarge': 4, 
+u'r4.xlarge': 4, 
+u'm1.small': 1, u'm3.2xlarge': 8, 
+u'c3.2xlarge': 8, u'c4.8xlarge': 36, u'c3.xlarge': 4, 'm1.large': 2, u'hs1.8xlarge': 16, u'c3.8xlarge': 32, u'c4.xlarge': 4, u'c3.4xlarge': 16, u't2.medium': 2, u'i2.8xlarge': 32, u'c4.4xlarge': 16, u't2.micro': 1, u'g2.2xlarge': 8, 
+u'c4.2xlarge': 8, u't2.small': 1, 'm1.medium': 1, 
+u'i2.xlarge': 4, u'm3.medium': 1, u'i2.4xlarge': 16, 
+u'c4.large': 2, u'i2.2xlarge': 8, u'c3.large': 2, 'm1.xlarge': 4, u'm3.xlarge': 4}
